4be0e43695104499fd7ff8da3822db07f895116a
krishanRsystems <149110389+krishanRsystems@users.noreply.github.com>
2023-10-27 15:12:54 +0530

    Update UnitTest1.cs
    updated camelCase

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -6,31 +6,31 @@ namespace TestPrimeReact
 {
     public class Tests
     {
-        private IWebDriver WebDriver { get; set; } = null;
-        private string DriverPath { get; set; } = Environment.CurrentDirectory;
-        private string BaseUrl { get; set; } = "https://www.primefaces.org/primereact-v5/#/datatable/selection";
+        private IWebDriver webDriver { get; set; } = null;
+        private string driverPath { get; set; } = Environment.CurrentDirectory;
+        private string baseUrl { get; set; } = "https://www.primefaces.org/primereact-v5/#/datatable/selection";
 
         [SetUp]
         public void Setup()
         {
-            WebDriver = GetChromeDriver();
-            WebDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(120);
-            WebDriver.Manage().Window.Maximize();
+            webDriver = GetChromeDriver();
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(120);
+            webDriver.Manage().Window.Maximize();
         }
 
         [TearDown]
         public void TearDown()
         {
-            WebDriver.Quit();
+            webDriver.Quit();
         }
 
         [Test]
         public void TestCheckListItem()
         {
-            WebDriver.Navigate().GoToUrl(BaseUrl);
-            Assert.AreEqual("PrimeReact",WebDriver.Title);
+            webDriver.Navigate().GoToUrl(baseUrl);
+            Assert.AreEqual("PrimeReact", webDriver.Title);
             Console.WriteLine("Title verification successful..!");
-            LandingPage landing = new LandingPage(WebDriver);
+            LandingPage landing = new LandingPage(webDriver);
             landing.CheckItem("Name", "Blue Band");
             //landing.CheckItem("Name", "Bracelet");
             //landing.CheckItem("Code", "244wgerg2");
@@ -39,7 +39,7 @@ namespace TestPrimeReact
         private WebDriver GetChromeDriver()
         {
             var options = new ChromeOptions();
-            return new ChromeDriver(DriverPath, options, TimeSpan.FromSeconds(300));
+            return new ChromeDriver(driverPath, options, TimeSpan.FromSeconds(300));
         }
     }
 }
