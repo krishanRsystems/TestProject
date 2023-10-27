e8695f194213e4dad305bfd577bf9c8937465577
krishanRsystems <149110389+krishanRsystems@users.noreply.github.com>
2023-10-27 15:17:34 +0530

    Update LandingPage.cs
    update

--- LandingPage.cs
+++ LandingPage.cs
@@ -15,10 +15,10 @@ namespace TestPrimeReact
 
         private IWebElement headerCheckbox => driver.FindElement(By.XPath("//h5[contains (text(), 'Checkbox')]"));
         private IWebElement checkboxTable => driver.FindElement(By.XPath("//h5[contains (text(), 'Checkbox')]/following-sibling::div[@class='p-datatable p-component']/div/table"));
-        private IList<IWebElement> columns => CheckboxTable.FindElements(By.XPath("thead/tr/th"));
-        private IList<IWebElement> tRows(int col) => CheckboxTable.FindElements(By.XPath($"tbody/tr/td[{col}]"));
-        private IList<IWebElement> itemCheckBox(int row) => CheckboxTable.FindElements(By.XPath($"tbody/tr[{row}]/td[1]"));
-        private IList<IWebElement> itemCheckBoxValue(int row) => CheckboxTable.FindElements(By.XPath($"tbody/tr[{row}]/td[1]//input"));
+        private IList<IWebElement> columns => checkboxTable.FindElements(By.XPath("thead/tr/th"));
+        private IList<IWebElement> tRows(int col) => checkboxTable.FindElements(By.XPath($"tbody/tr/td[{col}]"));
+        private IList<IWebElement> itemCheckBox(int row) => checkboxTable.FindElements(By.XPath($"tbody/tr[{row}]/td[1]"));
+        private IList<IWebElement> itemCheckBoxValue(int row) => checkboxTable.FindElements(By.XPath($"tbody/tr[{row}]/td[1]//input"));
 
         public void CheckItem(string header, string item)
         {
@@ -45,7 +45,7 @@ namespace TestPrimeReact
                     int colCount = columns.Count;
                     for (int c = 0; c < colCount; c++)
                     {
-                        if (!string.IsNullOrEmpty(columns[c].Text) && Header.Equals(columns[c].Text))
+                        if (!string.IsNullOrEmpty(columns[c].Text) && header.Equals(columns[c].Text))
                         {
                             colId = c + 1;
                             break;
@@ -57,7 +57,7 @@ namespace TestPrimeReact
                         for (int r = 0; r < rowCount; r++)
                         {
                             string row = tRows(colId)[r].Text;
-                            if (!string.IsNullOrEmpty(tRows(colId)[r].Text) && Item.Equals(tRows(colId)[r].Text))
+                            if (!string.IsNullOrEmpty(tRows(colId)[r].Text) && item.Equals(tRows(colId)[r].Text))
                             {
                                 rowId = r + 1;
                                 break;
